7836ba6c32ffac20be78c305a3fe26cadbe2c9c9
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-06-26 09:40:08 +0530

    modified ftp and made verbose

--- FTP/Clientftp.c
+++ FTP/Clientftp.c
@@ -11,12 +11,10 @@ int main(void)
 
     int socket_desc;
     struct sockaddr_in server_addr;
-    char server_message[2000];
-    char filename[2000];
+    char server_message[2000], filename[2000];
 
     // Clean buffers:
     memset(server_message, '\0', sizeof(server_message));
-    // memset(client_message,'\0',sizeof(client_message));
 
     // Create socket:
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -33,32 +31,33 @@ int main(void)
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     // Send connection request to server:
-
     if (connect(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         printf("Unable to connect\n");
         return -1;
     }
     printf("Connected with server successfully\n");
+
+    // Read a file name to send it to server to access
     printf("Enter a filename:");
     scanf("%s", filename);
 
-    // printf("Enter message: ");
-    // gets(client_message);
     if (send(socket_desc, filename, strlen(filename), 0) < 0)
     {
         printf("Unable to send message\n");
         return -1;
     }
 
+    // Receive the contents of the file as server_message
     if (recv(socket_desc, server_message, sizeof(server_message), 0) < 0)
     {
         printf("Error while receiving server's msg\n");
         return -1;
     }
-
+    // Display the file contents
     printf("File contents:\n %s\n", server_message);
 
+    // Close the connection
     close(socket_desc);
     return 0;
 }
--- FTP/Serverftp.c
+++ FTP/Serverftp.c
@@ -8,43 +8,45 @@
 int main()
 {
 
-	int socketfd, client_socket, client_size;
+	int socket_desc, client_socket, client_size;
 	struct sockaddr_in server_addr, client_addr;
 	char server_message[2000], client_message[2000];
 
+	// clear the buffer
 	memset(server_message, '\0', sizeof(server_message));
 	memset(client_message, '\0', sizeof(client_message));
 
-	socketfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (socketfd < 0)
+	// create a socket
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_desc < 0)
 	{
 		printf("Error while creating socket\n");
 		return -1;
 	}
-
 	printf("Socket created successfully\n");
 
+	// configure the family, port and address of server and bind
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(2000);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	if (bind(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	if (bind(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 	{
 		printf("Couldn't bind to the port\n");
 		return -1;
 	}
 	printf("Done with binding\n");
 
-	if (listen(socketfd, 0) < 0)
+	// listen to connections from the client
+	if (listen(socket_desc, 0) < 0)
 	{
 		printf("Cannot listen");
 		return -1;
 	}
 
-	// printf("\nWaiting for incoming connections\n");
+	// accept the clients connection
 	client_size = sizeof(client_addr);
-	client_socket = accept(socketfd, (struct sockaddr *)&client_addr, &client_size);
+	client_socket = accept(socket_desc, (struct sockaddr *)&client_addr, &client_size);
 
 	if (client_socket < 0)
 	{
@@ -53,43 +55,49 @@ int main()
 	}
 
 	printf("Client connected at IP:%s and port:%i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
+	
+	// receive message from the client
 	if (recv(client_socket, client_message, sizeof(client_message), 0) < 0)
 	{
 		printf("Couldn't receive\n");
 		return -1;
 	}
-
+	// display the file name received from client in client_message
 	printf("%s\n", client_message);
+
+	// open the file if valid and send its contents or else display and send error message
 	FILE *fileptr = fopen(client_message, "r");
 	if (fileptr == NULL)
 	{
 		strcpy(server_message, "NOT A VALID FILE");
 		if (send(client_socket, server_message, strlen(server_message), 0) < 0)
 		{
-			printf("Cant send\n");
+			printf("Couldn't send the error message\n");
 			return -1;
 		}
+		printf("File not send as it is not valid\n");
 		return -1;
 	}
 
 	int i = 0;
 	while (!feof(fileptr))
 	{
+		// store the contents of file in server_message array
 		server_message[i++] = fgetc(fileptr);
 	}
 
 	server_message[i] = '\0';
 	printf("\n%s", server_message);
 
+	// send the file contents
 	if (send(client_socket, server_message, strlen(server_message), 0) < 0)
 	{
-		printf("Cant send\n");
+		printf("Couldn't send the file\n");
 		return -1;
 	}
 
 	fclose(fileptr);
 	close(client_socket);
-	close(socketfd);
+	close(socket_desc);
 	return 0;
 }
