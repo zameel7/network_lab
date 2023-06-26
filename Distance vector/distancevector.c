dad6176df4759e46a8e6126ffde26095f5c31698
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-06-26 09:25:07 +0530

    verbose dvr and display fix

--- Distance vector/distancevector.c
+++ Distance vector/distancevector.c
@@ -1,6 +1,7 @@
 // Distance Vector Routing Protocol
-
 #include <stdio.h>
+
+// node to keep track of distance from one node to all other nodes
 struct node
 {
     unsigned dist[20];
@@ -12,16 +13,21 @@ int main()
     int costmat[20][20];
     int nodes, i, j, k, count = 0;
 
+    // accept no. of nodes
     printf("\nEnter the number of nodes:");
     scanf("%d", &nodes);
 
+    // accept the cost matrix
     printf("\nEnter the cost matrix:\n");
     for (i = 0; i < nodes; i++)
     {
         for (j = 0; j < nodes; j++)
         {
+            // accept i->j cost in costmat[i][j]
             scanf("%d", &costmat[i][j]);
+            // make diagonal costs 0 (no self loop)
             costmat[i][i] = 0;
+            // in node, distance from i->j is stored in rt[i].dist[j] through j is stored in rt[i].from[j]
             rt[i].dist[j] = costmat[i][j];
             rt[i].from[j] = j;
         }
@@ -29,10 +35,12 @@ int main()
 
     do
     {
+        // count used to keep track of changes, and if no changes after complete for loop iteration: it stops
         count = 0;
         for (i = 0; i < nodes; i++)
             for (j = 0; j < nodes; j++)
                 for (k = 0; k < nodes; k++)
+                    // if i->j distance is greater than i->k->j, update the node values in rt node struct
                     if (rt[i].dist[j] > costmat[i][k] + rt[k].dist[j])
                     {
                         // calculate minimum distance
@@ -42,12 +50,13 @@ int main()
                     }
     } while (count != 0);
 
+    // displaying distance from each node to the other via which node
     for (i = 0; i < nodes; i++)
     {
         printf("\n\n For router %d\n", i + 1);
         for (j = 0; j < nodes; j++)
         {
-            printf("\t\n Node %d via %d Distance %d", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
+            printf("\t\n Distance to node: %d via node: %d = %d", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
         }
     }
     printf("\n\n");
