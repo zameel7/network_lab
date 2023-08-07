95c97fa20540b2c4445cdfcd206dcee4b6cc2222
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2023-08-07 18:22:36 +0530

    modify s&w

--- Stop and Wait/stopandwait.c
+++ Stop and Wait/stopandwait.c
@@ -29,29 +29,24 @@ int main() {
 void sender() {
     static int flag=0;
     if(turn=='s') {
-        if (errorack==0)
+        if (!errorack)
         {
-            printf("Sender: sent packet with seq no.: %d\n", p.seq);
-            errorframe = rand()%4;
-            printf("%s\n", (errorframe==0?"Error while sending packet": ""));
-            turn = 'r';
-        }
-        else {
-            if(flag==1) printf("Sender: Received ACK for packet %d\n", ack);
+            if(flag==1) 
+                printf("Sender: Received ACK for packet %d\n", ack);
             if(p.seq==5) {
                 disconnect = 1;
                 return;
             }
             p.info += 1;
             p.seq += 1;
-            printf("Sender: sent packet with seq no.: %d\n", p.seq);
-            errorframe = rand()%4;
-            printf("%s\n", (errorframe==0?"Error while sending packet":""));
-            turn = 'r';
+        } else {
             flag = 1;
         }
-    }
-    else {
+        printf("Sender: sent packet with seq no.: %d\n", p.seq);
+        errorframe = rand()%4;
+        printf("%s\n", (errorframe == 0 ? "Error while sending packet" : ""));
+        turn = 'r';
+    } else {
         t--;
         printf("Sender time reducing \n");
         if(t==0) {
@@ -65,18 +60,18 @@ void sender() {
 void receiver() {
     static int frexp = 1;
     if(turn=='r') {
-        if(errorframe!=0) {
-            if(p.seq==frexp) {
+        if(errorframe != 0) {
+            if(p.seq == frexp) {
                 printf("Receiver: received packet with seq no.: %d\n", p.seq);
                 ack=p.seq;
                 frexp+=1;
                 turn='s';
                 errorack=rand()%4;
-                printf("%s\n", (errorack==0?"Error while sending ACK":""));
+                printf("%s\n", (errorack == 0 ? "Error while sending ACK" : ""));
             }
         }
         else {
-            printf("Receiver: Dupicated packet with seq %d\n", frexp-1);
+            printf("Receiver: Duplicated packet with seq %d\n", frexp-1);
             ack = frexp-1;
             turn = 's';
             errorack = rand()%4;
@@ -84,5 +79,3 @@ void receiver() {
         }
     }
 }
-
-
